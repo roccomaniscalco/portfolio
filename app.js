1839f2983efd5e67f6a833a57370f062d6853cf8
Rocco <roccomaniscalco2001@gmail.com>
2020-12-30 05:00:10 -0500

    design improvements to landing page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const hamburger = document.querySelector("#hamburger");
   const nav = document.querySelector("nav");
 
+  // TOGGLE MENU
   hamburger.addEventListener("click", () => {
     nav.classList.toggle("open");
     hamburger.classList.toggle("open");
